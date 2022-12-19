f2e908834a507c1d710bd5bcf667cae9a5e17504
Dawitlior <dawitlior777@gmail.com>
2022-12-19 19:33:12 +0200

    add

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3001/assets/${image}`}
+        src={`https://social-media-front-w6ow.onrender.com/assets/${image}`}
       />
     </Box>
   );
