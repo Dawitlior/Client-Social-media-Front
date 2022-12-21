767566ab5d169d786ac2bbd0ab6a9df4360bae3e
Dawitlior <dawitlior777@gmail.com>
2022-12-21 13:23:02 +0200

    add

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -23,7 +23,7 @@ const UserWidget = ({ userId, picturePath }) => {
 
   const getUser = async () => {
     const response = await fetch(
-      `https://backend-server-social-media.onrender.com/profile/${userId}`,
+      `https://backend-server-social-media.onrender.com/users/${userId}`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
